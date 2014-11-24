ca44ab2d1d294290d9408e7a89d631e5f2dd5908
Josh Levinger <josh@levinger.net>
2014-11-24 16:32:18 -0500

    add close button

--- js/main.js
+++ js/main.js
@@ -99,9 +99,13 @@ jQuery( document ).ready(function( $ ) {
         trackEvent('share');
     });
 
+    $('a.close').click(function (e){
+        $('.overlay').removeClass('visible');
+    })
+
 });
 
-function validateEmail(email) { 
+function validateEmail(email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
-} 
+}
