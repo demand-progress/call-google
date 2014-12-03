41364b44913a78f1e75fc18d985917e3d38c575d
Josh Levinger <josh@levinger.net>
2014-12-03 13:09:36 -0800

    include hashtag in example tweet

--- _includes/share_tweet_text.js
+++ _includes/share_tweet_text.js
@@ -3,4 +3,4 @@
 //  So instead of "Bill O'Reilly" you would write "Bill O\'Reilly".
 //  If you fail to do this, everything will be ruined.
 //
-TWEET_TEXT = 'Tell @googlepubpolicy @google that it\'s time for them to act like they really support #NetNeutrality. http://callongoogle.net'
+TWEET_TEXT = 'Tell @googlepubpolicy @google that it\'s time for them to act like they really support #NetNeutrality. http://callongoogle.net #CallOnGoogle'
